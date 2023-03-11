c76eeaca00fc7b764f600d08868b1af8d80031d2
John Doe <johndoe@email.com>
2023-03-11 09:31:11 +0400

    delete waste files

--- scrypt.js
+++ scrypt.js
@@ -18,6 +18,18 @@ const generateCatCard = (cat) => {
       </div>`
   )
 }
+const formDataFromLC = localStorage.getItem(document.forms.add_cats_form.name);
+const parsedData = formDataFromLC ? JSON.parse(formDataFromLC) : null;
+if (parsedData) {
+    Object.keys(parsedData).forEach(key => {
+        document.forms.add_cats_form[key].value = parsedData[key];
+    });
+}
+
+document.forms.add_cats_form.addEventListener('input', event =>{
+    const formData = Object.fromEntries(new FormData(document.forms.add_cats_form).entries());
+    localStorage.setItem(document.forms.add_cats_form.name, JSON.stringify(formData))
+});
 
 $wrapper.addEventListener('click', async (event) => {
   const action = event.target.dataset.action;
